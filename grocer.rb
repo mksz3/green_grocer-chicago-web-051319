3ff73a904358b2f520b92efbf002b0d597f78416
Markus Sanders <markus.sanderst@gmail.com>
2019-05-07 22:12:37 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,11 @@
 def consolidate_cart(cart)
   output = {}
-  cart.each {|item|
-    if output[item] == nil
-      output[item][count] = 0
-    else
-      output[item][count] += 1
-    end
+  cart.each { |itemObj|
+    itemObj.each { |itemName, saleInfo|
+      output[itemName] == nil ? output[itemName] = saleInfo.merge({:count => 1}) : output[itemName][:count] += 1
+    }
   }
-  output 
+  output
 end
 [
   {"AVOCADO" => {:price => 3.0, :clearance => true }},
